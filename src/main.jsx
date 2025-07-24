7908cf9beb65034edc5bad8838460d37d98cf15e
NJ-brothers <njbrothers229@gmail.com>
2025-07-24 07:27:11 +0500

    Soem issues solved

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,4 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
-
-
-createRoot(document.getElementById('root2')).render(
-  <StrictMode>
-    <App2 />
-  </StrictMode>,
-)
+);
